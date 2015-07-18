2450b138713e997656936e58776cff42b9c25b3c
Lucas Moura <lucas.moura128@gmail.com>
2015-07-18 03:32:34 -0300

    Creating script for displayng the digits
    The script number_visualization.py asks for an input that represents a row on
    the train.csv file and display the digit on the screen.
    
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- visualization/number_visualization.py
+++ visualization/number_visualization.py
@@ -1,9 +1,25 @@
 import csv
 import numpy as np
+import scipy.misc as sm
+import scipy.ndimage as nd
+
+def createImage(digit):
+
+    digit_image = np.zeros((28, 28))
+    size = 28
+
+    for row in range(0, size-1):
+        for column in range(0, size-1):
+            index = row*size +column
+            digit_image[row][column] = digit[index]
+
+    return digit_image        
+
 
 #Opening csv file and loading it into a numpy array
-with open('../data/dummy.csv', 'rb') as data:
+with open('../data/train.csv', 'rb') as data:
     reader = csv.reader(data, delimiter=',')
+    next(reader)
     numbers = np.array(list(reader)).astype(int)
 
 '''
@@ -13,3 +29,13 @@ with open('../data/dummy.csv', 'rb') as data:
 digits = numbers[:, [0]]
 handwritten = numbers[:,1:]
 
+
+print("Enter the row from the csv file that you which to see the digit")
+num_image = input()
+num_image-=1
+
+#Displaying the digit image accordingly to the user input
+digit_image = createImage(handwritten[num_image]) 
+digit_image = nd.zoom(digit_image, 8, order=3)
+img = sm.toimage(digit_image)
+img.show()
